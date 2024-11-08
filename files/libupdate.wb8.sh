d2806077b5027f3dadaddba799a73c7318fb6cd2
vdromanov <vdromanov@yahoo.com>
2024-11-08 17:28:31 +0300

    wb8.5x: enable buzzer in initramfs (wb8.4x compatible too)

--- files/libupdate.wb8.sh
+++ files/libupdate.wb8.sh
@@ -1,32 +1,68 @@
 #!/bin/bash
 
 POWER_BUTTON_GPIO=234
-PWM_BUZZER=1
 
 # 3 kHz, 50% volume
 DUTY_CYCLE=166666
 PERIOD=333333
 
-buzzer_init() {
-    echo $PWM_BUZZER > /sys/class/pwm/pwmchip0/export 2>/dev/null
 
+# Bootlet should be compatible with both WB8.4 & WB8.5+ devices
+# Since WB8.5, buzzer is available via embedded-controller & wbec-pwm driver
+# => enable both (wbec & t507) buzzer PWMs in bootlet dts and guess pwmchip & pwmbuzzer onthefly
+
+get_possible_pwmchips() {
+    local pwmchips=""
+    for compat in "allwinner,sun50i-t507-pwm" "wirenboard,wbec-pwm"; do
+        pwmchips="$pwmchips $(grep -H $compat /sys/class/pwm/pwmchip*/device/of_node/compatible | grep -Eo "pwmchip[[:digit:]]+")"
+    done
+    echo $pwmchips
+}
+
+# both wbec & non-wbec pwm drivers have hardcoded pwm outputs (unavailable to change via dts)
+get_pwm_buzzer() {
+    local pwmchip=$1
+    local compat=$(tr -d '\0' < "/sys/class/pwm/$pwmchip/device/of_node/compatible")
+    case $compat in
+        "wirenboard,wbec-pwm")
+            echo "0"
+            ;;
+        *)
+            echo "1"
+            ;;
+    esac
+}
+
+buzzer_init() {
     local r1=1
     local r2=1
     while [ $r1 -ne 0 ] || [ $r2 -ne 0 ]; do
-        echo $DUTY_CYCLE > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/duty_cycle 2>/dev/null
-        r1=$?
+        for pwmchip in $(get_possible_pwmchips); do
 
-        echo $PERIOD > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/period 2>/dev/null
-        r2=$?
+            local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
+            echo "$pwm_buzzer" > /sys/class/pwm/$pwmchip/export 2> /dev/null || true
+
+            echo "$PERIOD" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/period 2> /dev/null
+            r1=$?
+
+            echo "$DUTY_CYCLE" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/duty_cycle 2> /dev/null
+            r2=$?
+        done
     done
 }
 
 buzzer_on() {
-    echo 1 > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/enable
+    for pwmchip in $(get_possible_pwmchips); do
+        local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
+        echo "1" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/enable 2> /dev/null
+    done
 }
 
 buzzer_off() {
-    echo 0 > /sys/class/pwm/pwmchip0/pwm${PWM_BUZZER}/enable
+    for pwmchip in $(get_possible_pwmchips); do
+        local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
+        echo "0" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/enable 2> /dev/null
+    done
 }
 
 button_init() {
