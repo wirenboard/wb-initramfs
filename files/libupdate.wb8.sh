f5e1731ff66212b19d57fc70d3c565e67c3ba41e
vdromanov <vdromanov@yahoo.com>
2024-11-12 01:21:52 +0300

    rework pwm chip & buzzer guessing

--- files/libupdate.wb8.sh
+++ files/libupdate.wb8.sh
@@ -9,60 +9,59 @@ PERIOD=333333
 
 # Bootlet should be compatible with both WB8.4 & WB8.5+ devices
 # Since WB8.5, buzzer is available via embedded-controller & wbec-pwm driver
-# => enable both (wbec & t507) buzzer PWMs in bootlet dts and guess pwmchip & pwmbuzzer onthefly
+# => enable both (wbec & t507) buzzer PWMs in bootlet dts and guess pwmchip & pwmbuzzer via WBEC hwrev
+get_pwmchip_pwmbuzzer() {
+    local pwm_buzzer
+    local pwm_chip
+    local compatible
 
-get_possible_pwmchips() {
-    local pwmchips=""
-    for compat in "allwinner,sun50i-t507-pwm" "wirenboard,wbec-pwm"; do
-        pwmchips="$pwmchips $(grep -H $compat /sys/class/pwm/pwmchip*/device/of_node/compatible | grep -Eo "pwmchip[[:digit:]]+")"
-    done
-    echo $pwmchips
-}
-
-# both wbec & non-wbec pwm drivers have hardcoded pwm outputs (unavailable to change via dts)
-get_pwm_buzzer() {
-    local pwmchip=$1
-    local compat=$(tr -d '\0' < "/sys/class/pwm/$pwmchip/device/of_node/compatible")
-    case $compat in
-        "wirenboard,wbec-pwm")
-            echo "0"
+    local wbec_hwrev=$(cat /sys/bus/spi/drivers/wbec/spi0.0/hwrev 2>/dev/null || true)
+    case $wbec_hwrev in
+        "85")
+            pwm_buzzer="0"
+            compatible="wirenboard,wbec-pwm"
             ;;
         *)
-            echo "1"
+            pwm_buzzer="1"
+            compatible="allwinner,sun50i-t507-pwm"
             ;;
     esac
+
+    pwm_chip=$(grep -H $compatible /sys/class/pwm/pwmchip*/device/of_node/compatible | grep -Eo "pwmchip[[:digit:]]+")
+    echo "$pwm_chip $pwm_buzzer"
 }
 
 buzzer_init() {
     local r1=1
     local r2=1
+
+    IFS=' ' read -r -a CHIP_BUZZER <<< "$(get_pwmchip_pwmbuzzer)"
+    local pwmchip="${CHIP_BUZZER[0]}"
+    local pwmbuzzer="${CHIP_BUZZER[1]}"
+
     while [ $r1 -ne 0 ] || [ $r2 -ne 0 ]; do
-        for pwmchip in $(get_possible_pwmchips); do
-
-            local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
-            echo "$pwm_buzzer" > /sys/class/pwm/$pwmchip/export 2> /dev/null || true
-
-            echo "$PERIOD" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/period 2> /dev/null
-            r1=$?
-
-            echo "$DUTY_CYCLE" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/duty_cycle 2> /dev/null
-            r2=$?
-        done
+        echo "$pwmbuzzer" > /sys/class/pwm/$pwmchip/export 2> /dev/null || true
+        echo "$PERIOD" > /sys/class/pwm/$pwmchip/pwm${pwmbuzzer}/period 2> /dev/null
+        r1=$?
+        echo "$DUTY_CYCLE" > /sys/class/pwm/$pwmchip/pwm${pwmbuzzer}/duty_cycle 2> /dev/null
+        r2=$?
     done
 }
 
 buzzer_on() {
-    for pwmchip in $(get_possible_pwmchips); do
-        local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
-        echo "1" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/enable 2> /dev/null
-    done
+    IFS=' ' read -r -a CHIP_BUZZER <<< "$(get_pwmchip_pwmbuzzer)"
+    local pwmchip="${CHIP_BUZZER[0]}"
+    local pwmbuzzer="${CHIP_BUZZER[1]}"
+
+    echo "1" > /sys/class/pwm/$pwmchip/pwm${pwmbuzzer}/enable 2> /dev/null
 }
 
 buzzer_off() {
-    for pwmchip in $(get_possible_pwmchips); do
-        local pwm_buzzer=$(get_pwm_buzzer $pwmchip)
-        echo "0" > /sys/class/pwm/$pwmchip/pwm${pwm_buzzer}/enable 2> /dev/null
-    done
+    IFS=' ' read -r -a CHIP_BUZZER <<< "$(get_pwmchip_pwmbuzzer)"
+    local pwmchip="${CHIP_BUZZER[0]}"
+    local pwmbuzzer="${CHIP_BUZZER[1]}"
+
+    echo "0" > /sys/class/pwm/$pwmchip/pwm${pwmbuzzer}/enable 2> /dev/null
 }
 
 button_init() {
